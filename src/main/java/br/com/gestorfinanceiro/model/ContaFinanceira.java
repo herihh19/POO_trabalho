c8419879a0bd885b1289f5a122e03b71aa485d3a
Herica Becker <118032350+herihh19@users.noreply.github.com>
2025-11-08 20:02:39 -0300

    semana 2

---
+++ src/main/java/br/com/gestorfinanceiro/model/ContaFinanceira.java
@@ -0,0 +1,37 @@
+package br.com.gestorfinanceiro.model;
+
+import br.com.gestorfinanceiro.exception.SaldoInsuficienteException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ContaFinanceira {
+    protected String nome;
+    protected double saldo;
+    protected Usuario proprietario;
+    protected List<Lancamento> lancamentos;
+
+    public ContaFinanceira(String nome, double saldoInicial, Usuario proprietario) {
+        this.nome = nome;
+        this.saldo = saldoInicial;
+        this.proprietario = proprietario;
+        this.lancamentos = new ArrayList<>();
+    }
+
+    public abstract void depositar(double valor);
+    public abstract void sacar(double valor) throws SaldoInsuficienteException;
+
+    public void adicionarLancamento(Lancamento lancamento) {
+        this.lancamentos.add(lancamento);
+    }
+    
+    public double getSaldo() { return saldo; }
+    public String getNome() { return nome; }
+    public Usuario getProprietario() { return proprietario; }
+    public List<Lancamento> getLancamentos() { return lancamentos; }
+
+    @Override
+    public String toString() {
+        return nome + ": R$" + String.format("%.2f", saldo);
+    }
+}
